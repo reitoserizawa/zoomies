af057928fbde67edad6fa067203d69053ae4a1e4
ReitoSerizawa <s.reitiger@gmail.com>
2022-06-21 10:04:16 -0400

    tentative homepage done

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -18,10 +18,12 @@ function HomePage () {
 
         <div class="homepage-container" style={{height:"28rem", backgroundColor: "white"}}>
             <div style={{width: "50%", margin:"left"}}>
-                <h3 style={{position: "absolute", top: "30%", padding:"3rem", paddingLeft: "5rem"}}>Check Out Dog Parks <i class="fa-solid fa-map-location-dot"></i></h3>
-                <p style={{position: "absolute", top: "40%", padding:"3rem", paddingLeft: "5rem"}}>We have a list of NYC's dog parks! You can check their location and capability!</p>
-                <p style={{position: "absolute", top: "45%", padding:"3rem",  paddingLeft: "5rem"}}>Is it too busy? Let's go to another park! (Don't forget to check out!)</p>
-                <button type="button" class="btn btn-outline-secondary" style={{position: "absolute", top: "70%", left: "25%"}}>See more</button>
+                <h3 style={{position: "absolute", top: "30%", padding:"3rem", paddingLeft: "7rem"}}>Check Out Dog Parks <i class="fa-solid fa-map-location-dot"></i></h3>
+                <p style={{position: "absolute", top: "40%", padding:"3rem", paddingLeft: "7rem"}}>We have a list of NYC's dog parks! You can check their location and capability!</p>
+                <p style={{position: "absolute", top: "45%", padding:"3rem",  paddingLeft: "7rem"}}>Is it too busy? Let's go to another park! (Don't forget to check out!)</p>
+                <NavLink to='/dog_parks' exact>
+                    <button type="button" class="btn btn-outline-secondary" style={{position: "absolute", top: "70%", left: "25%"}}>See more</button>
+                </NavLink>
             </div>
             
             <div style={{width: "50%",  float:"right"}}>
@@ -30,11 +32,13 @@ function HomePage () {
         </div>
 
         <div class="homepage-container" style={{height:"28rem", backgroundColor: "#F5F5F5"}}>
-            <div style={{width: "50%", margin:"right"}}>
-                <h3 style={{position: "absolute", top: "30%", padding:"3rem", paddingLeft: "5rem"}}>Check Our Dog Parks <i class="fa-solid fa-map-location-dot"></i></h3>
-                <p style={{position: "absolute", top: "40%", padding:"3rem", paddingLeft: "5rem"}}>There is a list of NYC's dog parks! You can check their location and capability!</p>
-                <p style={{position: "absolute", top: "45%", padding:"3rem", paddingLeft: "5rem"}}>Is it too busy? Let's go to another park! (Don't forget to check out!)</p>
-                <button type="button" class="btn btn-outline-secondary" style={{position: "absolute", top: "70%", right:"25%"}}>See more</button>
+            <div style={{width: "50%", float:"right"}}>
+                <h3 style={{position: "absolute", top: "30%", padding:"3rem", paddingLeft: "7rem"}}>See Zoomie Members <i class="fa-solid fa-dog"></i></h3>
+                <p style={{position: "absolute", top: "40%", padding:"3rem", paddingLeft: "7rem"}}>Do you want to see our members? Check out this page!</p>
+                <p style={{position: "absolute", top: "45%", padding:"3rem", paddingLeft: "7rem"}}>You can check our doggie's profiles and make sure if they are friendly or not.</p>
+                <NavLink to='/dogs' exact>
+                    <button type="button" class="btn btn-outline-secondary" style={{position: "absolute", top: "70%", right:"25%"}}>See more</button>
+                </NavLink>
             </div>
             
             <div style={{width: "50%",  margin:"left"}}>
