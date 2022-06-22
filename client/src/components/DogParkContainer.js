08fc1d6805c288cd2b34c3511be066482cbdf0f5
ReitoSerizawa <s.reitiger@gmail.com>
2022-06-22 10:45:00 -0400

    edited

--- client/src/components/DogParkContainer.js
+++ client/src/components/DogParkContainer.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import { Routes, Route, useParams } from 'react-router-dom';
 import DogParkItem from './DogParkItem'
 
 function DogParkContainer ({isLoggedIn}) {
